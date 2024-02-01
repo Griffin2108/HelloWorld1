c5c16bbea4a5df992a3d85abaaba8502dcc8ccb5
Griffin2108 <gefreudenb@eagles.usi.edu>
2024-02-01 13:31:45 -0600

    Simple changes

--- AppLearn/wwwroot/js/site.js
+++ AppLearn/wwwroot/js/site.js
@@ -2,4 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
